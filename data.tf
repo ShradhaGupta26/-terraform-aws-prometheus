ab27c70c9d8c10cf546cbc714ea5c83ae22b8f83
ShradhaGupta26 <88206733+ShradhaGupta26@users.noreply.github.com>
2023-05-23 11:03:49 +0530

    Add files via upload

---
+++ data.tf
@@ -0,0 +1,18 @@
+data "aws_ami" "amazon-linux" {
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+  owners = ["amazon"]
+}
+
+data "template_file" "promscript" {
+template = file("${path.module}/script.sh")
+vars = {
+    pip = var.instance_private_ip
+    mpip = var.mongo_private_ip
+
+}
+}
+
---
+++ variable.tf
@@ -0,0 +1,2 @@
+variable "instance_private_ip" { }
+variable "mongo_private_ip" {}
